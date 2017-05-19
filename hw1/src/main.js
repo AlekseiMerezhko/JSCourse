2c0641971ce6f65e246bceaa1ba0ef2f72bd4030
alexey <aleksei.merezhko@mail.ru>
2017-05-19 07:58:10 +0300

    first commit

---
+++ hw1/src/main.js
@@ -0,0 +1,14 @@
+var user1 = {
+	name: 'Alex',
+	age: 17,
+	phonnumber: 0932960212,
+	contacts: {
+		skype: 'bladester428',
+		mail: 'aleksei.merezhko@mail.ru'
+	},
+	access: true,
+	othernumbers: [0932960212, 0932960212, 0932960212] 
+
+};
+
+console.log(user1);
